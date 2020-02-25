4483cbaf9b7b85c040866393d1fea05a7224d429
Dmitriy Savranin <dsavranin@gmail.com>
2020-02-25 15:20:00 +0500

    refactor: add my solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,42 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {	
+	if (date === undefined) {
+		return 'Unable to determine the time of year!';
+	}
+
+	try {
+		let m = date.getTime();
+		}
+	catch (err) {
+		throw new Error('Дата некорректна');
+	}
+	
+	let month = date.getMonth();
+	let res = '';
+	switch (month) {
+		case 11:
+		case 0:
+		case 1:
+		   res = 'winter';
+		   break;
+		   
+		case 2:
+		case 3:
+		case 4:
+		   res = 'spring';
+		   break; 
+		   
+		case 5:
+		case 6:
+		case 7:
+		   res = 'summer';
+		   break; 
+
+		case 8:
+		case 9:
+		case 10:
+		   res = 'autumn';
+		   break; 
+	}
+	
+	return res;
 };
