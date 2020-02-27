378587436fcb61dedf4e8316ad280e55a1705561
Dmitriy Savranin <dsavranin@gmail.com>
2020-02-27 17:47:14 +0500

    refactor: add solution of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,49 +1,43 @@
 module.exports = function transform(arr) {
-if (Array.isArray(arr) != true) {
-	throw new Error("This is not array!");
-}
-	
-newarr =[];
-
-for (let i = 0; i < arr.length; i++) {
-let elem = arr[i];
-	if (arr[i] ==  '--discard-next') {
-	
-	
+	if (Array.isArray(arr) != true) {
+		throw new Error("This is not array!");
 	}
-	else if (arr[i] == '--discard-prev') {
+	
+	newarr =[];
 
-	}
-	else if (arr[i] == '--double-next') {
-		if (i == arr.length-1) {
+	for (let i = 0; i < arr.length; i++) {
+		let elem = arr[i];
+		if (arr[i] ==  '--discard-next') {	
+	
+		}
+		else if (arr[i] == '--discard-prev') {
+			newarr.pop();
+		}
+		else if (arr[i] == '--double-next') {
+			if (i == arr.length-1) {
 			
+			}
+			else {
+				newarr.push(arr[i+1]);	
+			}
+		}
+		else if (arr[i] == '--double-prev') {
+			if (i > 0) {
+				newarr.push(arr[i-1]);
+			}
 		}
 		else {
-			newarr.push(arr[i+1]);	
-		}
-	}
-	else if (arr[i] == '--double-prev') {
-		if (i > 0) {
-			newarr.push(arr[i-1]);
-		}
-	}
-	else {
-		if (arr[i+1] == '--discard-prev') {
+			if (arr[i-1] == '--discard-next') {
 		
-		}
-		else if (arr[i-1] == '--discard-next') {
-		
-		}
-		else if (arr[i+1] == '--double-prev') {
-			newarr.push(arr[i]);				
-		}
-
-		else {
-			newarr.push(arr[i]);	
+			}
+			else if (arr[i+1] == '--double-prev') {
+				newarr.push(arr[i]);				
+			}
+			else {
+				newarr.push(arr[i]);	
+			}
 		}
 	}
-}
-
 
 return newarr;
 };
